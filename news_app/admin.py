bdd7972017c77028dd4e4c06ba55227bfd229aa1
Arnodt95 <arno.dt@voltano.com>
2026-02-06 10:39:52 +0200

    Detlete All files and Upload New Files

--- news_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Publisher, Article, Newsletter
-
-admin.site.register(CustomUser)
-admin.site.register(Publisher)
-admin.site.register(Article)
-admin.site.register(Newsletter)
